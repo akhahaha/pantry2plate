3af8f1a24e16c7763971f549233dc5fa6ca6be0f
Jeremy Kao <j7kao@ucsd.edu>
2014-04-12 15:03:48 -0700

    Adding button stuff for alan to copy

--- LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/MainActivity.java
+++ LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/MainActivity.java
@@ -1,9 +1,12 @@
 package com.lahacksrecipeapp.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button showRecipesButton = (Button) findViewById(R.id.showRecipesButton);
+
+        showRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent swag = new Intent(MainActivity.this, ShowRecipesActivity.class);
+                startActivity(swag);
+            }
+        });
     }
 
 
