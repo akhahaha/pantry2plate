bd62c06cc3490b2e81a900d12759c590d23125ce
Alan Kha <akhahaha@gmail.com>
2014-04-12 20:44:50 -0700

    Ingredients Add
    Implemented "Add Ingredients" action and text dialog.
    
    TODO:
    - Move add button out of action menu.
    - Implement "Get Recipes" button.
    - Implement item removal.

--- LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/MainActivity.java
+++ LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +36,6 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -45,8 +43,8 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // Has here. The action bar will
+        // aundle action bar item clicktomatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
--- LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/SelectIngredientsActivity.java
+++ LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/SelectIngredientsActivity.java
@@ -1,40 +1,89 @@
 package com.lahacksrecipeapp.app;
 
-import android.app.ListActivity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 
 /**
  * Created by Alan on 4/12/14.
  */
-public class SelectIngredientsActivity extends ListActivity {
+public class SelectIngredientsActivity extends ActionBarActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectingredients);
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // initialize ingredients list
-        ArrayList<String> items = new ArrayList<String>();
+        items = new ArrayList<String>();
         items.add("Eggs");
         items.add("Flour");
         items.add("Chocolate");
 
         // initialize adapter with array
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+        adapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, items);
 
         // setup listView
-        setListAdapter(adapter);
+        ListView lv = (ListView) findViewById(R.id.ingredientsListView);
+        lv.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.select_ingredients, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+            // prompt for user input
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle("Add Ingredient");
+            // alert.setMessage("Message");
+
+            // set an EditText view to get user input
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    items.add(input.getText().toString());
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+            return true;
+        }
+        else if (id == R.id.action_settings) {
+            // TODO: list settings?
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
