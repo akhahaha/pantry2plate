f9515cef82adc7ee57a6b781a6d3bc68547fcd1b
Alan Kha <akhahaha@gmail.com>
2014-04-13 01:03:22 -0700

    Ingredients Edit
    Ingredients long-press to edit implemented.

--- LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/SelectIngredientsActivity.java
+++ LAHacksRecipeApp/app/src/main/java/com/lahacksrecipeapp/app/SelectIngredientsActivity.java
@@ -2,11 +2,15 @@ package com.lahacksrecipeapp.app;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -30,6 +34,8 @@ public class SelectIngredientsActivity extends ActionBarActivity {
         items.add("Eggs");
         items.add("Flour");
         items.add("Chocolate");
+        for (int i = 0; i < 20; i++)
+            items.add("test"+i);
 
         // initialize adapter with array
         adapter = new ArrayAdapter<String>
@@ -38,6 +44,49 @@ public class SelectIngredientsActivity extends ActionBarActivity {
         // setup listView
         ListView lv = (ListView) findViewById(R.id.ingredientsListView);
         lv.setAdapter(adapter);
+
+        // long press listView
+        lv.setLongClickable(true);
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick (AdapterView<?> av, View v, int position, long id) {
+                // create alert dialog
+                final AlertDialog.Builder alert =
+                        new AlertDialog.Builder(SelectIngredientsActivity.this);
+                alert.setTitle("Edit Ingredient");
+                final int pos = position;
+
+                // set an EditText view to get user input
+                final EditText input = new EditText(SelectIngredientsActivity.this);
+                input.setText(items.get(pos));
+                alert.setView(input);
+
+                alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        items.set(pos, input.getText().toString());
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                alert.show();
+                return true;
+            }
+        });
+
+        // Get Recipes button
+        Button showRecipesButton = (Button) findViewById(R.id.showRecipesButton);
+        showRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SelectIngredientsActivity.this, ShowRecipesActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -54,17 +103,15 @@ public class SelectIngredientsActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_add) {
-            // prompt for user input
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
+            // create alert dialog
+            final AlertDialog.Builder alert = new AlertDialog.Builder(this);
             alert.setTitle("Add Ingredient");
-            // alert.setMessage("Message");
 
             // set an EditText view to get user input
             final EditText input = new EditText(this);
             alert.setView(input);
 
-            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
                     items.add(input.getText().toString());
                 }
